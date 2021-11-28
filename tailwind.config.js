dc022ab92adfe37c96486940bd0c0b6efe1e86b8
Jesse Maingi <maingijesse@gmail.com>
2021-11-28 13:57:20 +0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./**/*.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        "strong-cyan": "hsl(171,66%,44%)",
+        "light-blue": "hsl(233,100%,69%)",
+        "dark-gray-blue": "hsl(210,10%,33%)",
+        "gray-blue": "hsl(201,11%,66%)",
+      },
+      backgroundImage: {
+        hero: "url(./images/bg-header-desktop.png)",
+      },
+      fontFamily: {
+        BaiJamjuree: "Bai Jamjuree, sans-serif",
+      },
+      container: {
+        center: true,
+        padding: "1rem",
+        screens: {
+          lg: "1440px",
+          xl: "1440px",
+          "2xl": "1440px",
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
